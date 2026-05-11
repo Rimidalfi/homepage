3ca0986d54952067c92006124878a2736c6ff1b8
Wladimir Janowitsch <rimidalf@tutamail.com>
2026-05-11 22:56:04 +0200

    blogpage image implementation

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 
-from wagtail.models import Page
+from wagtail.models import Page, Orderable
 from wagtail.fields import StreamField, RichTextField
 from wagtail import blocks
 from wagtail.admin.panels import FieldPanel
@@ -18,6 +19,13 @@ class BlogIndexPage(Page):
         FieldPanel("intro"),
     ]
 
+    # changing context with custom Query
+    def get_context(self, request):
+        context = super().get_context(request)
+        blogpages = self.get_children().live().order_by("-first_published_at")
+        context["blogpages"] = blogpages
+        return context
+
 
 class BlogPage(Page):
     author = models.CharField(max_length=255)
@@ -34,4 +42,18 @@ class BlogPage(Page):
         FieldPanel("author"),
         FieldPanel("date"),
         FieldPanel("body"),
+        "gallery_images",
     ]
+
+
+class BlogPageGalleryImage(Orderable):
+    """"""
+
+    page = ParentalKey(
+        to=BlogPage, on_delete=models.CASCADE, related_name="gallery_images"
+    )
+    image = models.ForeignKey(
+        to="wagtailimages.Image", on_delete=models.CASCADE, related_name="+"
+    )
+    caption = models.CharField(max_length=255, blank=True)
+    panels = ["image", "caption"]
